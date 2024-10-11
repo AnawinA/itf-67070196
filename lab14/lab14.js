6b663d592d7cdd2047f6ef9b97cec5cea5bf010d
AnawinA <anawin29sep@gmail.com>
2024-10-11 13:41:16 +0700

    forgot to save

--- lab14/lab14.js
+++ lab14/lab14.js
@@ -8,6 +8,7 @@ const tableInsert = document.getElementById('table-insert')
 const nameInsert = document.getElementById('name-insert')
 const telInsert = document.getElementById('tel-insert')
 
+
 username.textContent = "Hello"
 
 function setName() {
@@ -20,20 +21,18 @@ function setProfile() {
 function addParagraph() {
     if (nameInsert.value && telInsert.value) {
         const newpara = document.createElement('tr')
-        newpara.innerHTML = `<td></td><td>${nameInsert.value}</td><td>${telInsert.value}</td>`
+        newpara.innerHTML = `<td mouseup=removeRow(e)></td><td>${nameInsert.value}</td><td>${telInsert.value}</td>`
         nameInsert.value = ""
         telInsert.value = ""
         tableInsert.appendChild(newpara)
     }
 }
-window.onload = () => {
-    document.querySelectorAll('tbody tr').forEach((row) => {
-        element.addEventListener('mouseup', function(event) {
-            if (event.button === 1) {
-              event.preventDefault()
-              row.remove()
-              console.log('hfidshif')
-            }
-        });
-    })
-}
+
+// function mouseup
+// element.addEventListener('mouseup', function(event) {
+//     if (event.button === 1) {
+//         event.preventDefault()
+//         row.remove()
+//         console.log('hfidshif')
+//     }
+// })
